604bb56df0d1aef115e4042545bc2f1b9f57717f
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2023-11-02 01:58:55 +0530

    third commit

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { signup } from '../api/api.js';
 import { useNavigate } from 'react-router-dom';
-import Authenticate from './otpverify.js';
-import axios from 'axios';
+import hide from '../assets/hide.png'; 
 
 const SignupPage = () => {
   const [fullName, setFullName] = useState('');
@@ -16,7 +14,6 @@ const SignupPage = () => {
   const [passwordMatch, setPasswordMatch] = useState(true);
   const history = useNavigate();
 
-
   const handleConfirmPasswordChange = (e) => {
     const confirmPasswordValue = e.target.value;
     setConfirmPassword(confirmPasswordValue);
@@ -28,160 +25,155 @@ const SignupPage = () => {
     }
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      // Call the login function to send data to the backend
-      const response = await axios.post('http://localhost:5000/signup', {fullName, companyName, email, role, department, password});
-      
-      if (response.data.token) {
-        localStorage.setItem('jwtToken', response.data.token);
-      }
-      //const verifyemail= Authenticate(email)
-      console.log('Login successful', response);
-      // Handle success, e.g., store user token in state or localStorage
-      console.log("===="+email)
-      history(`/authenticate/${email}`)
-    } catch (error) {
-      console.error('Login error', error);
-      // Handle error, e.g., display error message to the user
-    }
+    // Your signup logic here
+
+    // After successful signup, you can redirect to the desired page, e.g., login page
+    history('/login');
   };
+
   return (
-    <div className="flex">
-      <div className="w-1/2 p-8">
+    <div className="flex flex-col lg:flex-row items-center justify-center">
+      <div className="flex items-end justify-center w-1/2 p-8">
         {/* Add your image or content here */}
       </div>
-      <div className="w-1/2 p-8">
-        <h1 className="text-2xl font-bold mb-4">Signup</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
-              Full Name
-            </label>
-            <input
-              required
-              type="text"
-              id="fullName"
-              className="mt-1 p-2 w-full shadow-lg border rounded-md"
-              placeholder="Enter your full name"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="companyName" className="block text-sm font-medium text-gray-700">
-              Company Name
-            </label>
-            <input
-              required
-              type="text"
-              id="companyName"
-              className="mt-1 p-2 w-full border shadow-lg rounded-md"
-              placeholder="Enter your company name"
-              value={companyName}
-              onChange={(e) => setCompanyName(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Email
-            </label>
-            <input
-              required
-              type="email"
-              id="email"
-              className="mt-1 p-2 w-full border shadow-lg rounded-md"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="role" className="block text-sm font-medium text-gray-700">
-              Role/Title
-            </label>
-            <input
-              required
-              type="text"
-              id="role"
-              className="mt-1 p-2 w-full border shadow-lg rounded-md"
-              placeholder="Enter your role/title"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="department" className="block text-sm font-medium text-gray-700">
-              Department
-            </label>
-            <input
-              required
-              type="text"
-              id="department"
-              className="mt-1 p-2 w-full border shadow-lg rounded-md"
-              placeholder="Enter your department"
-              value={department}
-              onChange={(e) => setDepartment(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              required
-              type={showPassword ? "text" : "password"}
-              id="password"
-              className="mt-1 p-2 w-full border shadow-lg rounded-md"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <div className={`mb-4 ${!passwordMatch ? 'border-red-500' : ''}`}>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-              {!passwordMatch && confirmPassword !=="" ? 'Confirm Password - Do not match!' : 'Confirm Password '}
-            </label>
-            <input
-              required
-              type={showPassword ? "text" : "password"}
-              id="confirmPassword"
-              placeholder="Confirm your password"
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              className={`mt-1 p-2 w-full shadow-lg rounded-md ${
-                !passwordMatch && confirmPassword !=="" ? 'ring-2 ring-red-500 focus:ring-2 focus:outline-none focus:ring-red-500' : 'border'
-              }`}
-            />
-          </div>
-
-          <div className="mb-4">
-            <input
-              required
-              type="checkbox"
-              id="showPassword"
-              className="mr-2"
-              checked={showPassword}
-              onChange={() => setShowPassword(!showPassword)}
-            />
-            <label htmlFor="showPassword" className="text-sm font-medium text-gray-700">
-              Show Password
-            </label>
-          </div>
-
-          <div className="mb-4">
-            <button type="submit" className={`bg-blue-500 text-white p-2 rounded-md ${passwordMatch ? '' : 'bg-gray-300 cursor-not-allowed'}`} disabled={!passwordMatch}>
-              Signup
-            </button>
-          </div>
-        </form>
+      <div className="w-full p-4 lg:w-1/2 flex items-center justify-center h-screen">
+        <div className="bg-white flex-col lg:flex-row rounded-lg shadow-lg p-2 lg:w-1/2 xl:w-1.5/2 xs:w-full sm:w-full overflow-y-auto">
+          <h1 className="text-2xl flex items-center justify-center font-bold mb-4">Signup</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4 flex flex-wrap">
+              <div className="w-1/2 pr-2">
+                <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
+                  Full Name
+                </label>
+                <input
+                  required
+                  type="text"
+                  id="fullName"
+                  className="mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400"
+                  placeholder="Enter your full name"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+              </div>
+              <div className="w-1/2 pl-2">
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-700">
+                  Company Name
+                </label>
+                <input
+                  required
+                  type="text"
+                  id="companyName"
+                  className="mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400"
+                  placeholder="Enter your company name"
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="mb-4 flex flex-wrap">
+              <div className="w-1/2 pr-2">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  required
+                  type="email"
+                  id="email"
+                  className="mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="w-1/2 pl-2">
+                <label htmlFor="role" className="block text-sm font-medium text-gray-700">
+                  Role/Title
+                </label>
+                <input
+                  required
+                  type="text"
+                  id="role"
+                  className="mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400"
+                  placeholder="Enter your role/title"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="mb-4 flex flex-wrap">
+              <div className="w-1/2 pr-2">
+                <label htmlFor="department" className="block text-sm font-medium text-gray-700">
+                  Department
+                </label>
+                <input
+                  required
+                  type="text"
+                  id="department"
+                  className="mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400"
+                  placeholder="Enter your department"
+                  value={department}
+                  onChange={(e) => setDepartment(e.target.value)}
+                />
+              </div>
+              <div className="w-1/2 pl-2">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                    required
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    className="mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <span
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="relative  right-0 flex items-center pr-2 cursor-pointer"
+                  >
+                   <img src={hide} alt="Eye" className="w-6 h-6" />
+                  </span>
+              </div>
+            </div>
+            <div className={`mb-4 ${!passwordMatch ? 'border-red-500' : ''}`}>
+              <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+                {!passwordMatch && confirmPassword !== "" ? 'Confirm Password - Do not match!' : 'Confirm Password '}
+              </label>
+              <input
+                required
+                type={showPassword ? "text" : "password"}
+                id="confirmPassword"
+                placeholder="Confirm your password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                className={`mt-1 p-2 w-full border-2 rounded-md shadow-lg focus:outline-none focus:ring-1 focus:shadow-sm focus:ring-gray-400 ${
+                  !passwordMatch && confirmPassword !== "" ? 'ring-2 ring-red-500 focus:ring-2 focus:outline-none focus:ring-red-500' : 'border'
+                }`}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                required
+                type="checkbox"
+                id="showPassword"
+                className="mr-2"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+              />
+              <label htmlFor="showPassword" className="text-sm font-medium text-gray-700">
+                Show Password
+              </label>
+            </div>
+            <div className="mb-4 flex items-center justify-center">
+              <button type="submit" className={`bg-blue-500 text-white p-2 rounded-md active:scale-95 hover:shadow-md hover:bg-blue-600`}>
+                Signup
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import user from '../assets/user.png';
+import { useNavigate } from 'react-router-dom';
 
 const Homepage = () => {
   const [userName, setUserName] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Add a state variable to track login status
+  const history = useNavigate();
 
   useEffect(() => {
     // Get user information using the JWT token
@@ -11,29 +14,43 @@ const Homepage = () => {
 
     if (token) {
       // Make an authenticated request to your server to get user data
-      console.log(token);
       axios.get('http://localhost:5000/userinfo', {
         headers: { Authorization: `Bearer ${token}` },
       })
         .then((response) => {
-            console.log(response)
           if (response.data && response.data.userName) {
             setUserName(response.data.userName);
+            setIsLoggedIn(true); // User is logged in
           } else {
             console.error('User data request error: Invalid response format');
           }
         })
         .catch((error) => console.error('User data request error', error));
+    } else {
+      setIsLoggedIn(false); // User is not logged in
     }
   }, []);
 
+  const handleLogout = () => {
+    // Perform the logout action, e.g., clear the JWT token and redirect to the login page.
+    localStorage.removeItem('jwtToken');
+    // Redirect to the login page or any other desired page
+    history('/login');
+  };
+
   return (
-    <div>
-        <div className="flex items-center ">
-        <img src={user} alt='userimg' />
-      <h1>Hi {userName} you are logged in !</h1>
-      </div>
-      {/* Other content for the homepage */}
+    <div className="min-h-screen flex flex-col items-center justify-center text-center">
+      {isLoggedIn ? ( // Conditionally render content if the user is logged in
+        <>
+          <button onClick={handleLogout} className="cursor-pointer">
+            <img src={user} alt="userimg" className="max-w-xs " />
+          </button>
+          <p className="text-6xl">Hi {userName} you are logged in!</p>
+        </>
+      ) : (
+        // Display a message if the user is not logged in
+        <p className="text-6xl">You are not logged in.</p>
+      )}
     </div>
   );
 };
